54b029bde841a4affe81149efbf779c0367bd1c3
venkatsaketh <vvsaketh24@gmail.com>
2023-08-04 14:54:16 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,12 @@
+// const heading =  React.createElement("h1",{id:"heading"},"Hello World from React!");
+
+const parent = React.createElement(
+    "div", { id: "parent" },
+    React.createElement(
+        "div", { id: "child" },
+        [React.createElement("h1", {}, "Iam h1"), React.createElement("h2", {}, "Iam h2")]
+    ))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
